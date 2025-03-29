c4ef61cee11c727e4eaf7f6f5e7abc2f155a43f1
bosco0808 <bosco0808@gmail.com>
2025-03-29 15:16:27 +0800

    Installed tsx for test running da server Finished test server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express, { Express, Request, Response, Application } from "express";
 
 const app = express();
+const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("test");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on ${port}`);
 });
